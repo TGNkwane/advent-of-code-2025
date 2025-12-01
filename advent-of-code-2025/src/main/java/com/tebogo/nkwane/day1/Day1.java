17e419c8234dbfa824efc382cb400f9b6557c64a
Tebogo Nkwane <Tebogo.Nkwane@standardbank.co.za>
2025-12-01 16:46:07 +0200

    Initial commit

---
+++ advent-of-code-2025/src/main/java/com/tebogo/nkwane/day1/Day1.java
@@ -0,0 +1,11 @@
+package com.tebogo.nkwane.day1;
+
+public class Day1 {
+  Integer solveA(String text) {
+    return 0;
+  }
+
+  Integer solveB(String text) {
+    return 0;
+  }
+}
---
+++ advent-of-code-2025/src/main/java/com/tebogo/nkwane/template/DayT.java
@@ -0,0 +1,11 @@
+package com.tebogo.nkwane.template;
+
+public class DayT {
+  Integer solveA(String text) {
+    return 0;
+  }
+
+  Integer solveB(String text) {
+    return 0;
+  }
+}
